60b14d7455f8670a7230e2981738492a01e27c72
Rafał Brzozowski <rafal.brzozowski.ok@gmail.com>
2024-02-27 10:16:37 +0100

    Pierwszy zapis do repozytorium Lab1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+
+
+package pl.PP;
+
+
+public class MojaPierwszaAplikacja {
+    public static void main(String[] args) {
+
+System.out.printf("Witaj PP");        // to see how IntelliJ IDEA suggests fixing it.
+
+        }
+    }
